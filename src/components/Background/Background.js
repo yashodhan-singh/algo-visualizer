4dcb2de26f68c0bcce668e4b4b8a6e7400f3a78a
Yashodhan Singh <yashodhansingh@outlook.com>
2022-08-31 18:10:04 -0400

    Background

---
+++ src/components/Background/Background.js
@@ -0,0 +1,30 @@
+import './Background.scss';
+
+function Background() {
+    return(
+        <ul class="Background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    );
+}
+
+export default Background;
