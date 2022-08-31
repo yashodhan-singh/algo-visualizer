45198a56082d2493681232ddbc0c9586d021194a
Yashodhan Singh <yashodhansingh@outlook.com>
2022-08-31 18:09:32 -0400

    File Structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Header from './components/Header/Header';
+import Background from './components/Background/Background';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Background/>
+    <Header/>
+  </div>
+
+  // <div className="Main">
+  //   <Selector/>
+  //   <Visualizer/>
+  // </div>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
