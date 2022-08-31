361a1bbd969148cb1ceb386794ee85e637edac1e
Yashodhan Singh <yashodhansingh@outlook.com>
2022-08-31 18:10:14 -0400

    Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header>
+        <h1 className="Header__Title"> Algorithm Visualizer </h1>
+        <h1 className="Header__sub"> by Yashodhan Singh </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
