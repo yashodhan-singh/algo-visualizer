20f3d81ecb755f6d4a87b66da8af19c6d5a5f52f
Yashodhan Singh <yashodhansingh@outlook.com>
2022-09-06 16:24:18 -0400

    Header Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ function Header() {
     <div className="Header">
       <header>
         <h1 className="Header__Title"> Algorithm Visualizer </h1>
-        <h1 className="Header__sub"> by Yashodhan Singh </h1>
       </header>
     </div>
   );
